dab48bbcb3660d906dce036db4acc8b63916f737
StrawFox64 <strawberryfox417@gmail.com>
2024-01-02 17:27:49 -0300

    add sdl2

---
+++ scripts/015-sdl2_psl1ght.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+# sdl2_psl1ght.sh by Naomi Peori (naomi@peori.ca)
+
+## Download the source code.
+wget --no-check-certificate https://github.com/StrawFox64/SDL2PSL1GHT -O sdl2_psl1ght.tar.gz
+
+## Create the build directory.
+rm -Rf sdl2_psl1ght && mkdir sdl2_psl1ght
+
+## Unpack the source code.
+tar --strip-components=1 --directory=sdl2_psl1ght -xvzf sdl2_psl1ght.tar.gz
+
+## Open directory.
+cd sdl2_psl1ght
+
+## Compile and install.
+./script.sh && ${MAKE:-make} && ${MAKE:-make} install
